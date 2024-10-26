6fd50c0b0cb785470394f7b665d578bbeb1e5f3c
nayon-coders <freelancer4747@gmail.com>
2024-10-26 11:15:30 +0600

    controller makeing....

--- lib/view/today_sales_management/controller/sales_controller.dart
+++ lib/view/today_sales_management/controller/sales_controller.dart
@@ -1,7 +1,24 @@
 import 'package:get/get.dart';
 class SalesController extends GetxController {
-  var tabIndex = 0.obs;
-  void changeTabIndex(int index) {
-    tabIndex.value = index;
+
+  RxBool isLoading = false.obs;
+  RxBool isEdit = false.obs;
+  RxBool isAdd = false.obs;
+  RxBool isDelete = false.obs;
+  RxBool isView = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    isLoading.value = false;
   }
+
+  //get all sales
+  void getSales() async {
+    isLoading.value = true;
+    await Future.delayed(const Duration(seconds: 2));
+    isLoading.value = false;
+  }
+
+
 }
--- lib/view/today_sales_management/screen/today.sales.list.view.dart
+++ lib/view/today_sales_management/screen/today.sales.list.view.dart
@@ -1,10 +1,12 @@
 import 'package:abs_office_management/view/today_sales_management/controller/sales_controller.dart';
+import 'package:abs_office_management/widgets/app_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../utility/app_color.dart';
 import '../widget/edit_button.dart';
 import '../widget/table/app_table.dart';
+import '../widget/table/table_body.dart';
 import '../widget/table/table_header.dart';
 import 'add_today_sales.dart';
 
@@ -22,54 +24,75 @@ class TodaySalesView extends GetView<SalesController> {
 
       ],
       row: Expanded(
-        child: ListView.separated(
-            itemCount: 10,
-            separatorBuilder: (_, __) => Container(height: 1.5, color: Colors.grey[300]),
-            itemBuilder: (context,index){
-              return SizedBox(
-                height: 60,
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                        width: 80,
-                        child: Text("\$100,000",style:const TextStyle(fontSize: 12,fontWeight: FontWeight.w600,color: AppColors.textBlack),)),
-                    SizedBox(
-                        width: 80,
-                        child: Text("\$40,500",style:const TextStyle(fontSize: 12,fontWeight: FontWeight.w600,color: AppColors.textBlack),)),
-                    SizedBox(
-                        width: 55,
-                        child: Text("\$40,500",style:const TextStyle(fontSize: 12,fontWeight: FontWeight.w600,color: AppColors.textBlack),)),
-                    SizedBox(
-                        width: 120,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            EditButton(
-                              bgColor: Color(0xFF1814F3),
-                              onClick: ()=>Get.to(()=>AddTodaySales()),
-                            ),
-                            EditButton(
-                                icon: Icons.remove_red_eye,
-                                bgColor: Color(0xFF15A144),
-                                onClick: (){}
-                            ),
-                            EditButton(
-                                icon: Icons.delete,
-                                bgColor: Color(0xFFFE5C73),
-                                onClick: (){}
-                            ),
+        child: Obx(() {
+          if(controller.isLoading.value){
+            return _buildLoadingWidget(); // Loading widget
+          }else{
+            return ListView.separated(
+                itemCount: 10,
+                separatorBuilder: (_, __) => Container(height: 1.5, color: Colors.grey[300]),
+                itemBuilder: (context,index){
+                  return SizedBox(
+                    height: 60,
+                    child:Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TableBody(
+                          width: 80,
+                          text: "\$40,500",
+                        ),
+                        TableBody(
+                          width: 80,
+                          text: "\$40,500",
+                        ),
+                        TableBody(
+                          width: 55,
+                          text: "\$40,500",
+                        ),
 
-                          ],
-                        )),
+                        SizedBox(
+                            width: 120,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                EditButton(
+                                  bgColor: Color(0xFF1814F3),
+                                  onClick: ()=>Get.to(()=>AddTodaySales()),
+                                ),
+                                EditButton(
+                                    icon: Icons.remove_red_eye,
+                                    bgColor: Color(0xFF15A144),
+                                    onClick: (){}
+                                ),
+                                EditButton(
+                                    icon: Icons.delete,
+                                    bgColor: Color(0xFFFE5C73),
+                                    onClick: (){}
+                                ),
 
-                  ],
-                ),
-              );
+                              ],
+                            )),
 
-            }),
+                      ],
+                    ),
+                  );
+
+                });
+          }
+          }
+        ),
       ),
     );
   }
+
+  ListView _buildLoadingWidget() {
+    return ListView.separated(
+              itemCount: 10,
+              separatorBuilder: (_, __) => Container(height: 1.5, color: Colors.grey[300]),
+              itemBuilder: (context,index){
+                return AppShimmerPro.circularShimmer(width: Get.width, height: 45, borderRadius: 10);
+              });
+  }
 }
+
